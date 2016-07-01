ae4957b1b131544afb398f55d86e2bc938ca242a
Cory Johnson <cory.r.johnson@cox.net>
2016-07-01 08:36:58 -0500

    HTML and PHP complete. Pre decoration file.

--- TTT.php
+++ TTT.php
@@ -7,41 +7,56 @@
 
 <?php 
 include 'turncontrol.php';
+global $Xwins, $Owins, $ties;
 $num = $_GET[num];
 $boardval = $_GET[boardval]; 
-echo $num;
-echo $boardval;
-
+$first = $_GET[first];
+$Xwins = $_GET[Xwins];
+$Owins = $_GET[Owins];
+$ties = $_GET[ties];
+print_r($_GET);
 ?>
- 
+
+
+<p><?php 
+echo winnercontrol($boardval, $num, $Xwins, $Owins, $ties);
+echo $Xwins;
+?></p>
+
 
 <body>
-<table>
+<table class="playboard">
 	<tr>
-		<td class="rightbotbord"> <?php isitempty(0,$boardval,$num);?> </td>
-
-		<td class="rightleftbotbord"> <?php isitempty(1,$boardval,$num);?> </td>
-
-		<td class="leftbotbord"> <?php isitempty(2,$boardval,$num);?> </td>
+		<td class="rightbotbord"> <?php isitempty(0,$boardval,$num,$first, $Xwins, $Owins, $ties);?> </td>
+		<td class="rightleftbotbord"> <?php isitempty(1,$boardval,$num,$first, $Xwins, $Owins, $ties);?> </td>
+		<td class="leftbotbord"> <?php isitempty(2,$boardval,$num,$first, $Xwins, $Owins, $ties);?> </td>
 	</tr>
 	<tr>
-		<td class="righttopbotbord"> <?php isitempty(3,$boardval,$num);?> </td>
-		<td class="allbord"> <?php isitempty(4,$boardval,$num);?> </td>
-		<td class="lefttopbotbord"> <?php isitempty(5,$boardval,$num);?> </td>
+		<td class="righttopbotbord"> <?php isitempty(3,$boardval,$num,$first, $Xwins, $Owins, $ties);?> </td>
+		<td class="allbord"> <?php isitempty(4,$boardval,$num,$first, $Xwins, $Owins, $ties);?> </td>
+		<td class="lefttopbotbord"> <?php isitempty(5,$boardval,$num,$first, $Xwins, $Owins, $ties);?> </td>
 	</tr>
 	<tr>
-		<td class="righttopbord"> <?php isitempty(6,$boardval,$num);?> </td>
-		<td class="rightlefttopbord"> <?php isitempty(7,$boardval,$num);?> </td>
-		<td class="lefttopbord"> <?php isitempty(8,$boardval,$num);?> </td>
+		<td class="righttopbord"> <?php isitempty(6,$boardval,$num,$first, $Xwins, $Owins, $ties);?> </td>
+		<td class="rightlefttopbord"> <?php isitempty(7,$boardval,$num,$first, $Xwins, $Owins, $ties);?> </td>
+		<td class="lefttopbord"> <?php isitempty(8,$boardval,$num,$first, $Xwins, $Owins, $ties);?> </td>
 	</tr>
 </table>
 
 
 <a href="whoisxo.php"> Reset? </a>
+<a href="TTT.php?num=<?php echo $first;?>&boardval=000000000&first=<?php echo $first;?>&Xwins=<?php echo $Xwins;?>&Owins=<?php echo $Owins?>&ties=<?php echo $ties?>"> Rematch? </a>
 
-<?php 
-if ($num > 10) {
-echo winnercontrol($boardval);} ?>
+<table>
+<tr> 
+	<td> <?php echo $Xwins; ?> </td> </tr>
+<tr> 
+	<td> <?php echo $Owins; ?> </td> </tr>
+<tr>
+	<td> <?php echo $ties; ?> </td> </tr>
+</table>
+
+
 </body>
 </html>
 
--- turncontrol.php
+++ turncontrol.php
@@ -24,22 +24,56 @@ function thequerystuff ($spot, $boardval, $num) {
 
 }
 
-function isitempty ($spot, $boardval, $num) {
 
-	if ($boardval[$spot] == 0) {
-		?>
-		<a href="TTT.php?num=<?php echo $num+1;?>&boardval=<?php echo thequerystuff($spot, $boardval, $num);?>"><div><?php echo xoro($spot,$boardval,$num);?></div></a> 
-		<?php ;
-	}
-	elseif ($boardval[$spot] != 0) {
+function isitempty ($spot, $boardval, $num, $first, $Xwins, $Owins, $ties) {
+	global $Xwins, $Owins, $ties;
+	
+	if ($boardval[$spot] != 0) {
 		?>
 		<div class="noback"><?php echo xoro($spot, $boardval, $boardval[$spot]); ?></div>	
-		<?php ;}		
+		<?php ;}
 
+	elseif (
+		$boardval[0] + $boardval[1] + $boardval[2] == 9 ||
+		$boardval[3] + $boardval[4] + $boardval[5] == 9 || 
+		$boardval[6] + $boardval[7] + $boardval[8] == 9 ||	
+		$boardval[0] + $boardval[3] + $boardval[6] == 9 ||	
+		$boardval[1] + $boardval[4] + $boardval[7] == 9 ||
+		$boardval[2] + $boardval[5] + $boardval[8] == 9 ||
+		$boardval[0] + $boardval[4] + $boardval[8] == 9 ||
+		$boardval[2] + $boardval[4] + $boardval[6] == 9) {
+		?> 
+		<div class="tiediv"> </div> <?php ;
+	}	
+	elseif (
+		$boardval[0] + $boardval[1] + $boardval[2] == 15 ||
+		$boardval[3] + $boardval[4] + $boardval[5] == 15 || 
+		$boardval[6] + $boardval[7] + $boardval[8] == 15 ||	
+		$boardval[0] + $boardval[3] + $boardval[6] == 15 ||	
+		$boardval[1] + $boardval[4] + $boardval[7] == 15 ||
+		$boardval[2] + $boardval[5] + $boardval[8] == 15 ||
+		$boardval[0] + $boardval[4] + $boardval[8] == 15 ||
+		$boardval[2] + $boardval[4] + $boardval[6] == 15) {
+		?> 
+		<div class="tiediv"> </div> <?php ;
+	}
+	elseif ($boardval[$spot] == 0) {
+		?>
+		<a href="TTT.php?num=<?php echo $num+1;?>
+		&boardval=<?php echo thequerystuff($spot, $boardval, $num);?>
+		&first=<?php echo $first;?>
+		&Xwins=<?php echo $Xwins;?>
+		&Owins=<?php echo $Owins;?>
+		&ties=<?php echo $ties?>"><div class="clickdiv"><?php echo xoro($spot,$boardval,$num);?></div></a> 
+		<?php ;
+	}
+
+	
 }
 
+function winnercontrol ($boardval, $num, $Xwins, $Owins, $ties) {
+	global $Xwins, $Owins, $ties;
 
-function winnercontrol ($boardval) {
 	if ($boardval[0] + $boardval[1] + $boardval[2] == 9 ||
 		$boardval[3] + $boardval[4] + $boardval[5] == 9 || 
 		$boardval[6] + $boardval[7] + $boardval[8] == 9 ||	
@@ -50,6 +84,8 @@ function winnercontrol ($boardval) {
 		$boardval[2] + $boardval[4] + $boardval[6] == 9
 		) {
 		echo "Winner is X!!!!";
+		$Xwins += 1;
+		echo $Xwins;
 	}
 	elseif (
 		$boardval[0] + $boardval[1] + $boardval[2] == 15 ||
@@ -62,12 +98,30 @@ function winnercontrol ($boardval) {
 		$boardval[2] + $boardval[4] + $boardval[6] == 15
 		) {
 		echo "Winner is O!!!!";
+		$Owins =+ 1;
+		echo $Owins;
 	}
 
-	else {
+	elseif ($num > 10)
+		{		
 		echo "YOU TIED!!!";
+		$ties += 1;
+		echo $ties;
 	}
+
+	else { 
+		if ($num%2 == 0) {
+			?> Place your X on the board <?php ;}
+		elseif ($num%2 != 0) {
+			?> Place your O on the board <?php ;}
+	;}
+
+	echo $Xwins;
+	echo $Owins;
+	echo $ties;
+
 }
+
 ?>
 
 
